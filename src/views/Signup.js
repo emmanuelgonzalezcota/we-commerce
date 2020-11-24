8162a098281aebde69fdfaf4ddb046aeb9a059ca
Emmanuel <39785872+emmanuelgonzalezcota@users.noreply.github.com>
2020-11-23 20:36:38 -0700

    React Created and Routes configured

---
+++ src/views/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Navbar from '../components/Navbar';
+
+function Signup(){
+
+    return(
+        <div>
+            {/* <Navbar /> */}
+            <h1>El Signup</h1>
+        </div>
+    )
+}
+
+export default Signup;
